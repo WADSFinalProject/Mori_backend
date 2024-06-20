35ff83880a159183f34576fa1551306804e637c1
Francesco Emmanuel <114067626+FrancescoEmmanuel@users.noreply.github.com>
2024-06-20 07:48:04 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ def create_access_token(db: Session, user_id: int, role: str, name: str) -> str:
         "sub": str(user_id),
         "name": name,
         "role": role,
-        "exp": datetime.now(tz=timezone.utc) + timedelta(minutes=1)
+        "exp": datetime.now(tz=timezone.utc) + timedelta(minutes=20)
     }
     
     if role == "Centra":
