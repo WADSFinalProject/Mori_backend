6c2bb4f3bbfc00df0d98d3bc7f868c841c4211de
Vandra <evandraharya777@gmail.com>
2024-06-26 01:56:28 +0700

    change centraid

--- dependencies.py
+++ dependencies.py
@@ -2,10 +2,13 @@ from fastapi import Depends, HTTPException, Request
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError
 from utils import verify_token
+import models, schemas, crud
+from database import get_db
+from sqlalchemy.orm import Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     if not token:
         raise HTTPException(status_code=401, detail="Not authenticated")
     
@@ -20,8 +23,17 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         name: str = payload.get("name")
         if name is None:
             raise HTTPException(status_code=401, detail="Invalid authentication credentials")
+
         token_data = {"id": user_id, "role": role, "name": name}
+
+        if role == "Centra":
+            user_centra = db.query(models.UserCentra).filter(models.UserCentra.userID == user_id, models.UserCentra.Active == True).first()
+            if user_centra is None:
+                raise HTTPException(status_code=401, detail="No active centralID found for the user")
+            token_data["centralID"] = user_centra.CentraID
+
         return token_data
+
     except JWTError as e:
         raise HTTPException(status_code=401, detail="Invalid token")
 
