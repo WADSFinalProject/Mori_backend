1f71df15434631c38a68374a10cd431634bdd269
Vandra <evandraharya777@gmail.com>
2024-06-27 01:16:46 +0700

    d

--- secured_routes.py
+++ secured_routes.py
@@ -12,17 +12,17 @@ secured_router = APIRouter()
 async def protected_route(user: dict = Depends(get_current_user)):
     return {"message": "You are authenticated", "user": user["sub"]}
 
-# @secured_router.get("/user", response_model=schemas.User)
-# def read_user(db: Session = Depends(get_db), current_user: dict = Depends(get_current_user)):
-#     user_id = current_user["id"]
-#     if user_id is None:
-#         raise HTTPException(status_code=400, detail="Invalid user ID")
+@secured_router.get("/user", response_model=schemas.User)
+def read_user(db: Session = Depends(get_db), current_user: dict = Depends(admin_user)):
+    user_id = current_user["id"]
+    if user_id is None:
+        raise HTTPException(status_code=400, detail="Invalid user ID")
 
-#     db_user = crud.get_user(db=db, user_id=user_id)
-#     if db_user is None:
-#         raise HTTPException(status_code=404, detail="User not found")
+    db_user = crud.get_user(db=db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
     
-#     return db_user
+    return db_user
     
 # Batches
 @secured_router.get("/batches/", response_model=List[schemas.ProcessedLeaves_DriedDate])
