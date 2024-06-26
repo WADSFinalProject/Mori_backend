be6854056fb953e87acf4680a441c736afbda082
Vandra <evandraharya777@gmail.com>
2024-06-26 23:55:38 +0700

    a

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import ast, os
 
 load_dotenv()
 
-KEY = (os.getenv("KEY"))  # Ensure this is correctly set in the .env file
+KEY = os.getenv("KEY") # Ensure this is correctly set in the .env file
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
